b79c32dd1b8bc649a4938a77c955d19d3c69f6f9
Ariel <arielbric4@gmail.com>
2021-10-21 14:00:34 -0700

    Created a keywords.txt file and added a few words. Also begain to test searching with these key words

--- main.py
+++ main.py
@@ -5,11 +5,17 @@ import flask
 with open("secret.txt") as file:
         clientid = file.readline
         clientsecret = file.readline
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=clientid,
-                                                           client_secret=clientsecret))
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="***REMOVED***", client_secret="***REMOVED***"))
 # Limit of the number in a search is 50, You can not search for more items than 50 at a time
-results = sp.search(q='Sagittarius', limit=50)
-for idx, track in enumerate(results['tracks']['items']):
-    print(idx, track['name'])
+#results = sp.search(q='Leo', limit=50)
+#for idx, track in enumerate(results['tracks']['items']):
+ #   print(idx, track['name'])
 
 print("Hello World")
+
+with open("keywords.txt") as keywordFile:
+    for keyword in keywordFile:
+        results = sp.search(q = keyword, limit=10)
+        print("\n",keyword)
+        for idx, track in enumerate(results['tracks']['items']):
+            print(idx, track['name'])
