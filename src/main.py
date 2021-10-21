aeaf1069a0d745037cba2b54241bb43fc5c0239f
Ariel <arielbric4@gmail.com>
2021-10-21 14:23:26 -0700

    small fixes

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import flask
 with open("secret.txt") as file:
         clientid = file.readline
         clientsecret = file.readline
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="***REMOVED***", client_secret="***REMOVED***"))
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=clientid, client_secret=clientsecret))
 # Limit of the number in a search is 50, You can not search for more items than 50 at a time
 #results = sp.search(q='Leo', limit=50)
 #for idx, track in enumerate(results['tracks']['items']):
