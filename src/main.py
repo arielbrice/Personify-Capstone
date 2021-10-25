3f7d1c0221b701e01a87e01533198985ac409557
Ariel <arielbric4@gmail.com>
2021-10-25 11:01:16 -0700

    Commit to pull

--- src/main.py
+++ src/main.py
@@ -7,15 +7,15 @@ with open("secret.txt") as file:
         clientsecret = file.readline
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=clientid, client_secret=clientsecret))
 # Limit of the number in a search is 50, You can not search for more items than 50 at a time
-#results = sp.search(q='Leo', limit=50)
+#results = sp.search(q='Cancer', limit=50)
 #for idx, track in enumerate(results['tracks']['items']):
- #   print(idx, track['name'])
+#    print("Cancer:",idx, track['name'])
 
 print("Hello World")
 
 with open("keywords.txt") as keywordFile:
     for keyword in keywordFile:
-        results = sp.search(q = keyword, limit=10)
+        results = sp.search(q = keyword, limit=50)
         print("\n",keyword)
         for idx, track in enumerate(results['tracks']['items']):
             print(idx, track['name'])
