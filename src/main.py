a51b8975f12eda98032fa5e8d470a5c2979d5963
Hannah Siitia <siitiahm@plu.edu>
2021-10-24 22:28:12 -0700

    Got authorization working without needing literal secret text.

--- src/main.py
+++ src/main.py
@@ -3,9 +3,7 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import flask
 
 with open("secret.txt") as file:
-        clientid = file.readline
-        clientsecret = file.readline
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=clientid, client_secret=clientsecret))
+    sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=file.readline(), client_secret=file.readline()))
 # Limit of the number in a search is 50, You can not search for more items than 50 at a time
 #results = sp.search(q='Leo', limit=50)
 #for idx, track in enumerate(results['tracks']['items']):
@@ -19,3 +17,4 @@ with open("keywords.txt") as keywordFile:
         print("\n",keyword)
         for idx, track in enumerate(results['tracks']['items']):
             print(idx, track['name'])
+    keywordFile.close()
