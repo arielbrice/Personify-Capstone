c8e6d75a55fbd2dd747a22fef82a8dfc785fdc70
Ariel <arielbric4@gmail.com>
2021-11-11 12:09:10 -0800

    Testing

--- src/main.py
+++ src/main.py
@@ -1,20 +1,31 @@
 import spotipy
+from spotipy import client
 from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 import flask
+import os;
+
 
 with open("secret.txt") as file:
-    sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=file.readline(), client_secret=file.readline()))
+   # print(file.readline(), file.readline())
+   clientid = file.readline()
+   clientsecret = file.readline()
+   print(clientid,clientsecret)
+   sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=clientid, client_secret=clientsecret))
 # Limit of the number in a search is 50, You can not search for more items than 50 at a time
 #results = sp.search(q='Cancer', limit=50)
 #for idx, track in enumerate(results['tracks']['items']):
 #    print("Cancer:",idx, track['name'])
 
-print("Hello World")
+#print(sp.artist("https://open.spotify.com/artist/6vWDO969PvNqNYHIOW5v0m?si=AWecjQtFSxK7gHyyOnDFTw"))
+print(sp.user("dakotajamesspears"))
+
 
 with open("keywords.txt") as keywordFile:
     for keyword in keywordFile:
-        results = sp.search(q = keyword, limit=50)
+        results = sp.search(q = keyword, limit=10)
         print("\n",keyword)
         for idx, track in enumerate(results['tracks']['items']):
             print(idx, track['name'])
-    keywordFile.close()
+
+            
