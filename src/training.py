d663e0835b0d9c5123f2ca73a15c9ee068acddd5
Ariel <arielbric4@gmail.com>
2022-03-29 10:42:47 -0700

    creaed a dataframe and only getting analysis now

--- src/training.py
+++ src/training.py
@@ -1,5 +1,9 @@
 #get items from the user mongo db
 import pymongo
+import sklearn
+from sklearn.cluster import KMeans
+from sklearn import metrics
+import pandas as pd
 
 with open("dbconnection.txt") as file:
     connectionstring = file.readline().strip()
@@ -10,6 +14,17 @@ mycol = mydb["Tracks"]
 db = []
 x = mycol.find()
 for data in x:
-    db.append(data['analysis'])
+    db.append(data['analysis'][3:])
 
-print(db)
+#print(db)
+
+
+#'title', 'artist', 'id','acousticness', 'danceability','energy','instrumentalness','key','liveness','loudness','mode','speechiness'
+
+#do a kmeans clustering on the data in db
+
+
+df = pd.DataFrame(db, columns=['acousticness', 'danceability','energy','instrumentalness','key','liveness','loudness','mode','speechiness'])
+
+print(df)
+km = KMeans(n_clusters = 5, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)
